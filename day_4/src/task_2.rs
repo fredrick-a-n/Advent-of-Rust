79ee64ab9d57428dc604de991e8b9941f96f31e2
fredrick.a.n <fredrick.n.317@gmail.com>
2022-12-04 10:18:59 +0100

    Day 4 complete

---
+++ day_4/src/task_2.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use std::path::Path;
+
+use regex::Regex;
+
+pub fn task_2(){
+    let contents = fs::read_to_string(Path::new("./input1"))
+        .expect("Should have been able to read the file");
+    let re = Regex::new(r"([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)").unwrap();
+    let mut count = 0;
+    for cap in re.captures_iter(&contents) {
+        let a = &cap[1].parse::<i32>().unwrap();
+        let b = &cap[2].parse::<i32>().unwrap();
+        let c = &cap[3].parse::<i32>().unwrap();
+        let d = &cap[4].parse::<i32>().unwrap();
+        if a <= d && c <= b{
+            count += 1;
+        } 
+    }
+    println!("{}", count);
+}
