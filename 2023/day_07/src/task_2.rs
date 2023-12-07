f56dde3695477cb8ab980209982494f80f24c221
fredrick.a.n <fredrick.n.317@gmail.com>
2023-12-07 17:31:08 +0100

    Day 07

---
+++ 2023/day_07/src/task_2.rs
@@ -0,0 +1,36 @@
+use itertools::Itertools;
+pub fn task_2() {
+    println!("Tasks 2: {}", include_str!("../input1").split("\n").map(|line| {
+        let (h, s) = line.split_once(" ").unwrap();
+        (Hand::new(h.chars().map(|c| match c {'T' => 10, 'J' => 1, 'Q' => 12, 'K' => 13, 'A' => 14, _ => c.to_digit(10).unwrap()}).collect()), 
+        s.parse::<i32>().unwrap())
+    }).sorted().enumerate().fold(0, |acc, (i, (_, bet))| acc + (i as i32+1)*bet))
+}   
+
+#[derive(PartialEq, Eq, Ord, PartialOrd)]
+enum Hand {
+    HighCard(u32),
+    OnePair(u32),
+    TwoPair(u32),
+    ThreeofaKind(u32),
+    FullHouse(u32),
+    FourofaKind(u32),
+    FiveofaKind(u32),
+}
+
+impl Hand {
+    fn new(cards: Vec<u32>) -> Hand {
+        let num = cards.iter().rev().enumerate().fold(0, |acc, (i, &x)| acc + x*(16_u32).pow(i as u32));
+        let (m,n) = (2..15).rev().map(|i| cards.iter().filter(|&x| *x == i).count() as u8)
+            .fold((0,0), |acc,j| if j > acc.0 {(j, acc.0)} else if j > acc.1 {(acc.0, j)} else {acc});
+        match (5.min(m + cards.iter().filter(|&x| *x == 1).count() as u8), n) {
+            (5, _) => Hand::FiveofaKind(num),
+            (4, _) => Hand::FourofaKind(num),
+            (3, 2) => Hand::FullHouse(num),
+            (3, 1) => Hand::ThreeofaKind(num),
+            (2, 2) => Hand::TwoPair(num),
+            (2, 1) => Hand::OnePair(num),
+            _ => Hand::HighCard(num),
+        }
+    }
+}
