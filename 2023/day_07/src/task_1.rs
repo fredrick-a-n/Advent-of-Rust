99ffd9326d71c72944cae14965bd26d45872b3f7
fredrick.a.n <fredrick.n.317@gmail.com>
2023-12-07 18:54:47 +0100

    Cleaned up day 7

--- 2023/day_07/src/task_1.rs
+++ 2023/day_07/src/task_1.rs
@@ -1,11 +1,12 @@
-use itertools::Itertools;
 pub fn task_1() {
-    println!("Tasks 1: {}", include_str!("../input1").split("\n").map(|line| {
-        let (h, s) = line.split_once(" ").unwrap();
-        (Hand::new(h.chars().map(|c| match c {'T' => 10, 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14, _ => c.to_digit(10).unwrap()}).collect()), 
-        s.parse::<i32>().unwrap())
-    }).sorted().enumerate().fold(0, |acc, (i, (_, bet))| acc + (i as i32+1)*bet))
+    println!("Tasks 1: {}", itertools::Itertools::sorted(include_str!("../input1").split("\n").map(|line| {
+        map_to_hand(line.split_once(" ").unwrap())
+    })).enumerate().fold(0, |acc, (i, (_, bet))| acc + (i as i32+1)*bet))
 }   
+
+fn map_to_hand((hand, bet) :(&str,&str)) -> (Hand, i32){
+    (Hand::new(hand.chars().map(|c| match c {'T' => 10, 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14, _ => c.to_digit(10).unwrap()}).collect()), bet.parse::<i32>().unwrap())
+}
 
 #[derive(PartialEq, Eq, Ord, PartialOrd)]
 enum Hand {
@@ -20,16 +21,23 @@ enum Hand {
 
 impl Hand {
     fn new(cards: Vec<u32>) -> Hand {
-        let num = cards.iter().rev().enumerate().fold(0, |acc, (i, &x)| acc + x*(16_u32).pow(i as u32));
-        match (2..15).rev().map(|i| cards.iter().filter(|&x| *x == i).count() as u8)
-            .fold((0,0), |acc,j| if j > acc.0 {(j, acc.0)} else if j > acc.1 {(acc.0, j)} else {acc}) {
-            (5, _) => Hand::FiveofaKind(num),
-            (4, 1) => Hand::FourofaKind(num),
-            (3, 2) => Hand::FullHouse(num),
-            (3, 1) => Hand::ThreeofaKind(num),
-            (2, 2) => Hand::TwoPair(num),
-            (2, 1) => Hand::OnePair(num),
-            _ => Hand::HighCard(num),
+        match get_max_counts(&cards) {
+            (5, _) => Hand::FiveofaKind(get_hexa(&cards)),
+            (4, _) => Hand::FourofaKind(get_hexa(&cards)),
+            (3, 2) => Hand::FullHouse(get_hexa(&cards)),
+            (3, 1) => Hand::ThreeofaKind(get_hexa(&cards)),
+            (2, 2) => Hand::TwoPair(get_hexa(&cards)),
+            (2, 1) => Hand::OnePair(get_hexa(&cards)),
+            _ => Hand::HighCard(get_hexa(&cards)),
         }
     }
+}
+
+fn get_max_counts(cards: &Vec<u32>) -> (u8, u8) {
+    (2..15).rev().map(|i| cards.iter().filter(|&x| *x == i).count() as u8)
+        .fold((0,0), |acc,j| if j > acc.0 {(j, acc.0)} else if j > acc.1 {(acc.0, j)} else {acc})
+}
+
+fn get_hexa(cards: &Vec<u32>) -> u32 {
+    cards.iter().rev().enumerate().fold(0, |acc, (i, &x)| acc + x*(16_u32).pow(i as u32))
 }
--- 2023/day_07/src/task_2.rs
+++ 2023/day_07/src/task_2.rs
@@ -1,11 +1,12 @@
-use itertools::Itertools;
 pub fn task_2() {
-    println!("Tasks 2: {}", include_str!("../input1").split("\n").map(|line| {
-        let (h, s) = line.split_once(" ").unwrap();
-        (Hand::new(h.chars().map(|c| match c {'T' => 10, 'J' => 1, 'Q' => 12, 'K' => 13, 'A' => 14, _ => c.to_digit(10).unwrap()}).collect()), 
-        s.parse::<i32>().unwrap())
-    }).sorted().enumerate().fold(0, |acc, (i, (_, bet))| acc + (i as i32+1)*bet))
+    println!("Tasks 2: {}", itertools::Itertools::sorted(include_str!("../input1").split("\n").map(|line| {
+        map_to_hand(line.split_once(" ").unwrap())
+    })).enumerate().fold(0, |acc, (i, (_, bet))| acc + (i as i32+1)*bet))
 }   
+
+fn map_to_hand((hand, bet) :(&str,&str)) -> (Hand, i32){
+    (Hand::new(hand.chars().map(|c| match c {'T' => 10, 'J' => 1, 'Q' => 12, 'K' => 13, 'A' => 14, _ => c.to_digit(10).unwrap()}).collect()), bet.parse::<i32>().unwrap())
+}
 
 #[derive(PartialEq, Eq, Ord, PartialOrd)]
 enum Hand {
@@ -20,17 +21,27 @@ enum Hand {
 
 impl Hand {
     fn new(cards: Vec<u32>) -> Hand {
-        let num = cards.iter().rev().enumerate().fold(0, |acc, (i, &x)| acc + x*(16_u32).pow(i as u32));
-        let (m,n) = (2..15).rev().map(|i| cards.iter().filter(|&x| *x == i).count() as u8)
-            .fold((0,0), |acc,j| if j > acc.0 {(j, acc.0)} else if j > acc.1 {(acc.0, j)} else {acc});
-        match (5.min(m + cards.iter().filter(|&x| *x == 1).count() as u8), n) {
-            (5, _) => Hand::FiveofaKind(num),
-            (4, _) => Hand::FourofaKind(num),
-            (3, 2) => Hand::FullHouse(num),
-            (3, 1) => Hand::ThreeofaKind(num),
-            (2, 2) => Hand::TwoPair(num),
-            (2, 1) => Hand::OnePair(num),
-            _ => Hand::HighCard(num),
+        match add_jokers(get_max_counts(&cards), &cards) {
+            (5, _) => Hand::FiveofaKind(get_hexa(&cards)),
+            (4, _) => Hand::FourofaKind(get_hexa(&cards)),
+            (3, 2) => Hand::FullHouse(get_hexa(&cards)),
+            (3, 1) => Hand::ThreeofaKind(get_hexa(&cards)),
+            (2, 2) => Hand::TwoPair(get_hexa(&cards)),
+            (2, 1) => Hand::OnePair(get_hexa(&cards)),
+            _ => Hand::HighCard(get_hexa(&cards)),
         }
     }
+}
+
+fn get_max_counts(cards: &Vec<u32>) -> (u8, u8) {
+    (2..15).rev().map(|i| cards.iter().filter(|&x| *x == i).count() as u8)
+        .fold((0,0), |acc,j| if j > acc.0 {(j, acc.0)} else if j > acc.1 {(acc.0, j)} else {acc})
+}
+
+fn add_jokers((a,b):(u8,u8), cards: &Vec<u32>) -> (u8,u8) {
+    (a + cards.iter().filter(|&x| *x == 1).count() as u8, b)
+}
+
+fn get_hexa(cards: &Vec<u32>) -> u32 {
+    cards.iter().rev().enumerate().fold(0, |acc, (i, &x)| acc + x*(16_u32).pow(i as u32))
 }
