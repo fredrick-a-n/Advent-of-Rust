742e61e7b01dd06db079ab8a88356a387a6d973f
fredrick.a.n <fredrick.n.317@gmail.com>
2023-12-09 15:06:51 +0100

    Day 9 cleanup

--- 2023/day_09/src/task_1.rs
+++ 2023/day_09/src/task_1.rs
@@ -1,17 +1,14 @@
 pub fn task_1() {
     println!("Task 1 {}",
         include_str!("../input1").lines()
-            .map(|line| line.split_whitespace()
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>()
-            )
-            .map(|line| {
-                std::iter::successors(Some((line, 0)), |(l, n)| 
+            .map(|line| 
+                std::iter::successors(Some((line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>(), 0)), |(l, _)| 
                     if l.iter().any(|x| *x != 0) {
                         Some((l.windows(2).map(|x| x[1] - x[0]).collect::<Vec<i32>>(), *l.last().unwrap()))
-                    } else {None}
+                    } else {
+                        None
+                    }
                 ).fold(0, |acc, (_, n)| acc+n )
-            })
-            .sum::<i32>()
+            ).sum::<i32>()
         )
 }
--- 2023/day_09/src/task_2.rs
+++ 2023/day_09/src/task_2.rs
@@ -1,22 +1,14 @@
 pub fn task_2() {
     println!("Task 2 {}",
-    include_str!("../input1").lines()
-        .map(|line| line.split_whitespace()
-            .map(|x| x.parse::<i32>().unwrap())
-            .collect::<Vec<i32>>()
+        include_str!("../input1").lines()
+            .map(|line| 
+                std::iter::successors(Some((line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).rev().collect::<Vec<i32>>(), 0)), |(l, _)| 
+                    if l.iter().any(|x| *x != 0) {
+                        Some((l.windows(2).map(|x| x[1] - x[0]).collect::<Vec<i32>>(), *l.last().unwrap()))
+                    } else {
+                        None
+                    }
+                ).fold(0, |acc, (_, n)| acc+n )
+            ).sum::<i32>()
         )
-        .map(|line|
-            line.iter().rev().map(|x| *x).collect::<Vec<i32>>()
-        )
-        .map(|line| {
-            std::iter::successors(Some((line, 0)), |(l, _)| 
-                if l.iter().any(|x| *x != 0) {
-                    Some((l.windows(2).map(|x| x[1] - x[0]).collect::<Vec<i32>>(), *l.last().unwrap()))
-                } else {
-                    None
-                }
-            ).fold(0, |acc, (_, n)| acc+n )
-        })
-        .sum::<i32>()
-    )
 }
